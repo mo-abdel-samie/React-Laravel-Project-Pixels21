8b988ae7970de0aa1e480fb19b99d17f5b88e23e
OmarAbdo14 <77195857+OmarAbdo14@users.noreply.github.com>
2021-04-23 11:53:26 +0200

    last editing

--- app/Http/Controllers/admin/CMSController.php
+++ app/Http/Controllers/admin/CMSController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Storage;
 
 class CMSController extends Controller
 {
+
     public function index() {
 
         return view('admin/index') ;
@@ -20,74 +21,171 @@ class CMSController extends Controller
         return view('admin.cms.sections', ['sections'=>$sections]) ;
     }
 
+    // array contains inputs of every section with input types
     public $sections = [
-        'about_header'=>[
-            'text'=>[
-                'info-title'=>'required',
-                'info-desc1'=>'required',
-                'info-desc2-left-title'=>'required',
-                'info-desc2-left-desc'=>'required',
-                'info-desc2-right-title'=>'required',
-                'info-desc2-right-desc'=>'required',
-                'facebook-icon'=>'required',
-                'twitter-icon'=>'required',
-                'youtube-icon'=>'required',
-                'linkedin-icon'=>'required',
-
+        'home_header'=> [
+            'text'=> [
+                'facebook-icon'=>'required|string',
+                'twitter-icon'=>'required|string',
+                'youtube-icon'=>'required|string',
+                'linkedin-icon'=>'required|string',
             ],
             'url'=>[
-                'facebook-link'=>'required',
-                'twitter-link'=>'required',
-                'youtube-link'=>'required',
-                'linkedin-link'=>'required',
-            ],
-        ],
-        'about'=>[
-            'text'=>[
-                'title'=>'required',
-                'desc'=>'required',
-                'mission_title'=>'required',
-                'mission_desc'=>'required',
-                'vision_title'=>'required',
-                'vision_desc'=>'required',
-            ],
-            'url'=>[
-                'video'=>'required',
+                'facebook-link'=>'required|url',
+                'twitter-link'=>'required|url',
+                'youtube-link'=>'required|url',
+                'linkedin-link'=>'required|url',
             ],
             'file'=>[
-                'image1'=>'required|file|image|mimes:jpg,jpeg,png|max:5000',
-                'image2'=>'required|file|image|mimes:jpg,jpeg,png|max:5000',
-                'image3'=>'required|file|image|mimes:jpg,jpeg,png|max:5000',
-                'image4'=>'required|file|image|mimes:jpg,jpeg,png|max:5000',
+                'background-image'=>'required|file|image|mimes:jpg,jpeg,png,svg,gif|max:5000',
+                'slogan-image'=>'required|file|image|mimes:jpg,jpeg,png,svg,gif|max:5000',
+            ],
+        ],
+        'home_slogan'=>[
+            'text'=>[
+                'title1'=>'required|string',
+                'icon1'=>'required|string',
+                'title2'=>'required|string',
+                'icon2'=>'required|string',
+                'title3'=>'required|string',
+                'icon3'=>'required|string',
             ],
 
+        ],
+        'home_about'=>[
+            'text'=>[
+                'section-title'=>'required|string',
+                'section-desc'=>'required|string',
+                'mission-title'=>'required|string',
+                'mission-desc'=>'required|string',
+                'vision-title'=>'required|string',
+                'vision-desc'=>'required|string',
+            ],
+            'url'=>[
+                'video'=>'required|url',
+            ],
+            'file'=>[
+                'image1'=>'required|file|image|mimes:jpg,jpeg,png,svg,gif|max:5000',
+                'image2'=>'required|file|image|mimes:jpg,jpeg,png,svg,gif|max:5000',
+                'image3'=>'required|file|image|mimes:jpg,jpeg,png,svg,gif|max:5000',
+                'image4'=>'required|file|image|mimes:jpg,jpeg,png,svg,gif|max:5000',
+            ],
+
+        ],
+        'home_statistics'=>[
+            'text'=> [
+                'icon1'=>'required|string',
+                'icon2'=>'required|string',
+                'icon3'=>'required|string',
+                'icon4'=>'required|string',
+                'title1'=>'required|integer',
+                'title2'=>'required|integer',
+                'title3'=>'required|integer',
+                'title4'=>'required|string',
+            ],
+        ],
+        'home_supervisor'=>[
+            'text'=>[
+                'section-title'=>'required|string',
+                'supervisor-name'=>'required|string',
+                'video-icon'=>'required|string',
+            ],
+            'file'=>[
+                'image1'=>'required|file|image|mimes:jpg,jpeg,png,svg,gif|max:5000',
+                'image2'=>'required|file|image|mimes:jpg,jpeg,png,svg,gif|max:5000',
+            ],
+        ],
+        'about_header'=>[
+            'text'=>[
+                'info-title'=>'required|string',
+                'info-desc1'=>'required|string',
+                'info-desc2-left-title'=>'required|string',
+                'info-desc2-left-desc'=>'required|string',
+                'info-desc2-right-title'=>'required|string',
+                'info-desc2-right-desc'=>'required|string',
+                'facebook-icon'=>'required|string',
+                'twitter-icon'=>'required|string',
+                'youtube-icon'=>'required|string',
+                'linkedin-icon'=>'required|string',
+            ],
+            'url'=>[
+                'facebook-link'=>'required|url',
+                'twitter-link'=>'required|url',
+                'youtube-link'=>'required|url',
+                'linkedin-link'=>'required|url',
+            ],
+            'file'=>[
+                'background-image'=>'required|file|image|mimes:jpg,jpeg,png,svg,gif|max:5000',
+            ],
         ],
         'about_slogan'=>[
             'text'=>[
-                'title'=>'required',
+                'title'=>'required|string',
             ],
             'file'=>[
                 'image'=>'required|file|image|mimes:jpg,jpeg,png|max:5000',
             ],
         ],
-        'slogan'=>[
+        'F-A-Q'=> [
             'text'=>[
-                'title1'=>'required',
-                'icon1'=>'required',
-                'title2'=>'required',
-                'icon2'=>'required',
-                'title3'=>'required',
-                'icon3'=>'required',
+                'section_title'=>'required|string',
+                'section_subtitle'=>'required|string',
+                'section_desc'=>'required|string',
+                'icon'=>'required|string',
             ],
-
+        ],
+        'projects_header'=>[
+            'text'=>[
+                'section-title'=>'required|string',
+            ],
+            'file'=>[
+                'projects-video'=>'mimetypes:video/avi,video/mp4,video/webm,video/mpeg,video/quicktime',
+                'background-image'=>'required|file|image|mimes:jpg,jpeg,png|max:5000',
+                'slogan-image'=>'required|file|image|mimes:jpg,jpeg,png|max:5000',
+            ],
+        ],
+        'courses'=>[
+            'text'=>[
+                'courses-title'=>'required|string',
+                'instructors-title'=>'required|string',
+            ],
+        ],
+        'course_item'=>[
+            'text'=>[
+                'course-link'=>'required|string',
+                'rate-icon'=>'required|string',
+            ],
+        ],
+        'footer'=>[
+            'text'=>[
+                'links-title'=>'required|string',
+                'links-icon'=>'required|string',
+                'section-desc1'=>'required|string',
+                'copy-rights'=>'required|string',
+                'facebook-icon'=>'required|string',
+                'twitter-icon'=>'required|string',
+                'youtube-icon'=>'required|string',
+                'linkedin-icon'=>'required|string',
+            ],
+            'url'=>[
+                'facebook-link'=>'required|url',
+                'twitter-link'=>'required|url',
+                'youtube-link'=>'required|url',
+                'linkedin-link'=>'required|url',
+            ],
+            'file'=>[
+                'logo-image'=>'required|file|image|mimes:jpg,jpeg,png,svg,gif|max:5000',
+            ],
         ],
     ];
 
     public function showSectionForm ($id) {
         $section = Content::find($id);
 //        dd($section->section_content);
+        // convert data into associative array
         $data = json_decode($section->section_content, true);
 //        dd(is_array($data));
+        // return section data and section of same name in sections array
         return view('admin.cms.form', ['id'=>$section->id, 'section_name'=>$section->section_name, 'data'=>$data, 'sectionInputTypes'=>$this->sections[$section->section_name],]);
     }
 
@@ -97,17 +195,19 @@ class CMSController extends Controller
         $section = Content::find($id);
         $sectionInputs = $this->sections[$section->section_name];
 
-        $inputTypes=[];
+//        $inputTypes=[];
         foreach ($sectionInputs as $type=>$inputs) {
             $inputType = $type;
             $$inputType = $inputs;
+            // get array contains array of inputs of each section type
             $data[]= $$inputType;
-            $inputTypes[$inputType] = $$inputType;
+//            $inputTypes[$inputType] += $$inputType;
         }
 
         $validated = [];
         if (count($data)>1) {
             for ($i=count($data)-1;$i>0;$i--) {
+                // Merge the data inputs in one array
                 $validated += array_merge($data[$i],$data[$i-1]);
             }
         } else {
